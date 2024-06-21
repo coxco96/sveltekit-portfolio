82705b2bbaf2e9e9b331c9d78cb4f67baa4f519a
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-06-21 19:56:46 -0400

    create card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    import mapPin from '$lib/images/map-pin.svg';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+    <span class="welcome">
+        <picture>
+            <img src={mapPin} alt="map pin" />
+        </picture>
+    </span>
+	<h1>
+		
+
+		Hi, I'm Court.<br />
+	</h1>
+
+	<p>
+		I design and develop <strong>data experiences</strong>.
+    </p>
+
+	<!-- <Counter /> -->
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 595 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
