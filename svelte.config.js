1a55ab37e1f983c05abb86cfe13cfa86f03e381c
Court Cox <82842603+coxco96@users.noreply.github.com>
2024-06-21 15:53:04 -0400

    change adapter assets to static folder

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
-			assets: 'build',
+			assets: 'static',
 			fallback: undefined,
 			precompress: false,
 			strict: true
